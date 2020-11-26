385c3dc625693bca78628c19ec57f2867b458eef
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2020-11-27 01:41:04 +0530

    Updated JawsDB Connection

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "pizzaria";
+    $host = "r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";  
+    $user = "trwa79u42cbawfe3";  
+    $password = 'j75t6k6iuafub68k';  
+    $db_name = "jukem88zsy77801x";
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
